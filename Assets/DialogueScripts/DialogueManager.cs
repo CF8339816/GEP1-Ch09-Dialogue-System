94d0f69745232963c620353b4d035145c9d8d3b7
CF8339816 <chris_french_3dga@hotmail.com>
2026-03-26 00:43:04 -0300

    built successfully  error was a left over using unuity renderer  entry from the tutorial.

--- Assets/DialogueScripts/DialogueManager.cs
+++ Assets/DialogueScripts/DialogueManager.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 
 
-//using Unity.VisualScripting;
+
+
+
 using UnityEngine;
-//using UnityEngine.UI;
+
 
 
 public class DialogueManager : MonoBehaviour
